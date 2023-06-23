b5394df6e740d3c1ab52bd4c9c68154136325fa3
Yoariht Macedo <yoariht.mc37@gmail.com>
2023-06-23 16:36:20 -0600

    solucion del error del babel.config.js con cambio de extension a babel.config.cjs

--- doc-cont/tests/base-pruebas/02-template-string.test.js
+++ doc-cont/tests/base-pruebas/02-template-string.test.js
@@ -1,4 +1,4 @@
-import { getSaludo } from "../../src/base-pruebas/02-template-string";
+import { getSaludo } from '../../src/base-pruebas/02-template-string';
 
 
 describe('Pruebas en 02-template-string', () => { 
